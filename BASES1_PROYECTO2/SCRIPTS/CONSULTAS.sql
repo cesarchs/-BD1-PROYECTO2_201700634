66a1eafc82306a6940ff587d513ddd4f8f194505
cesarchs <58505340+cesarchs@users.noreply.github.com>
2021-10-30 12:16:21 -0600

    FUNCIONAL
    CONSULTA 2 Y DISTINTAS  CONSULTAS

--- BASES1_PROYECTO2/SCRIPTS/CONSULTAS.sql
+++ BASES1_PROYECTO2/SCRIPTS/CONSULTAS.sql
@@ -1,4 +1,4 @@
-
+##################################################################################################################################################
 #CONSULTA #1
 /*
 Desplegar para cada elección el país y el partido político que obtuvo mayor
@@ -6,14 +6,7 @@ porcentaje de votos en su país. Debe desplegar el nombre de la elección, el
 año de la elección, el país, el nombre del partido político y el porcentaje que
 obtuvo de votos en su país.
 */
-USE BASES1P2;
 
-####################################################################################################
-
-select * from partido;
-
-
-#SOLO FALTA TOMAR EL MAS GRANDE
 SELECT nomb_ele as NOMBRE_ELECCION, anio_ele AS AÑO_ELECCION , paiss AS PAIS, par_nomb AS NOMBRE_PARTIDO, suma*100/total AS PORCENTAJE
 FROM (
 SELECT pais.pais as paiss, eleccion.nombre_eleccion as nomb_ele, eleccion.anio_eleccion AS anio_ele, partido.nombre_partido as par_nomb, partido.partido as part_part , sum(poblacion.alfabeto + poblacion.analfabeto) as suma
@@ -62,62 +55,59 @@ order by par_nomb
 ;
 
 
+############################################################################################################################################################
+#CONSULTA # 2
+/*
+Desplegar total de votos y porcentaje de votos de mujeres por departamento
+y país. El ciento por ciento es el total de votos de mujeres por país. (Tip:
+Todos los porcentajes por departamento de un país deben sumar el 100%)
+*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-select eleccion.nombre_eleccion, eleccion.anio_eleccion, pais.pais, partido.nombre_partido , max(suma) as VOTOS
-FROM
-(
-	SELECT pais.id_pais as paiss, eleccion.id_eleccion as id_ele, partido.id_partido as par_id, sum(poblacion.alfabeto + poblacion.analfabeto) as suma
-	FROM pais, region, depto, municipio,
-		poblacion,
-		eleccion, municipio_eleccion,
-		partido, municipio_partido
-	where 	#pais hasta municipio
-			pais.id_pais = region.fk_id_pais
-		and region.id_region = depto.fk_id_region
-		and depto.id_depto = municipio.fk_id_depto
-		# poblacion
-		and poblacion.fk_id_municipio = municipio.id_municipio
-		# eleccion, municipio eleccion
-		and municipio_eleccion.fk_id_municipio = municipio.id_municipio
-		and municipio_eleccion.fk_id_eleccion = eleccion.id_eleccion
-		# partido, municipio partido
-		and municipio_partido.fk_id_municipio = municipio.id_municipio
-		and municipio_partido.fk_id_partido = partido.id_partido
-		and partido.id_partido = poblacion.fk_id_partido
-	group by partido.nombre_partido
-    order by  suma desc
-
-) as tabled , pais, eleccion, partido
-WHERE 
-			tabled.paiss = pais.id_pais
-        and tabled.id_ele = eleccion.id_eleccion
-        and tabled.par_id = partido.id_partido
-        
-	group by  PAIS.PAIS
+SELECT paiss AS PAIS , deptoo AS DEPARTAMENTO , votos as TOTAL_VOTOS , (votos*100/total) AS PORCENTAJE
+FROM (
+		select pais as paiss, region as regionn, depto as deptoo, sexo.sexo as sexoo, sum(poblacion.alfabeto + poblacion.analfabeto) as votos
+		from pais, region, depto, municipio,
+			 poblacion,
+			 sexo, detalle_sexo
+		where 	#comprobacion desde pais hasta municipio 
+				pais.id_pais = region.fk_id_pais
+			and region.id_region = depto.fk_id_region
+			and municipio.fk_id_depto = depto.id_depto
+			
+				# POBLACION 
+			and poblacion.fk_id_municipio = municipio.id_municipio
+				
+				# SEXO Y DETALLE SEXO
+			and detalle_sexo.fk_id_poblacion = poblacion.id_poblacion
+			and detalle_sexo.fk_id_sexo = sexo.id_sexo
+			and sexo.sexo = 'mujeres'
+			group by depto.depto
+			order by id_poblacion
+    ) a,
+	(
+		select pais as paisss, sum(poblacion.alfabeto + poblacion.analfabeto) as total
+		from pais, region, depto, municipio,
+			 poblacion,
+			 sexo, detalle_sexo
+		where 	#comprobacion desde pais hasta municipio 
+				pais.id_pais = region.fk_id_pais
+			and region.id_region = depto.fk_id_region
+			and municipio.fk_id_depto = depto.id_depto
+			
+				# POBLACION 
+			and poblacion.fk_id_municipio = municipio.id_municipio
+				
+				# SEXO Y DETALLE SEXO
+			and detalle_sexo.fk_id_poblacion = poblacion.id_poblacion
+			and detalle_sexo.fk_id_sexo = sexo.id_sexo
+			and sexo.sexo = 'mujeres'
+			group by pais.pais
+	) b
+    where a.paiss = b.paisss
 ;
 
 
-####################################################################################################
+
 
 
 # PAIS CON LOS PARTIDOS POR PAIS
@@ -156,7 +146,7 @@ where pais.id_pais = region.fk_id_pais
 
 
 
-
+use bases1p2
 # pais con cantidad de votos por pais
 select eleccion.nombre_eleccion, eleccion.anio_eleccion , pais, sum(poblacion.analfabeto + poblacion.alfabeto) as votos
 from pais, region, depto, municipio, poblacion, eleccion, municipio_eleccion #, partido, partido_eleccion, municipio_partido, poblacion_eleccion
@@ -167,14 +157,6 @@ where pais.id_pais = region.fk_id_pais
     #eleccion 
     and municipio_eleccion.fk_id_municipio = municipio.id_municipio
     and municipio_eleccion.fk_id_eleccion = eleccion.id_eleccion
-    # partido 
-    #and partido_eleccion.fk_id_eleccion = eleccion.id_eleccion
-    #and partido_eleccion.fk_id_partido = partido.id_partido
-   # and municipio_partido.fk_id_municipio = municipio.id_municipio
-   # and municipio_partido.fk_id_partido = partido.id_partido
-    # poblacion eleccion
-   # and poblacion_eleccion.fk_id_poblacion = poblacion.id_poblacion
-   # and poblacion_eleccion.fk_id_eleccion = eleccion.id_eleccion
 group by pais
 ;
 
@@ -182,25 +164,6 @@ group by pais
 
 
 
-
-select PA AS PAISS, PO AS poblacion, max(suma) as maximo
-FROM
-(
-SELECT  pais.pais AS PA, poblacion.id_poblacion AS PO, sum(poblacion.analfabeto + poblacion.alfabeto) as suma 
-FROM  pais, region, depto, municipio, poblacion
-WHERE pais.id_pais = region.fk_id_pais
-	AND region.id_region = depto.fk_id_region
-    AND depto.id_depto = municipio.fk_id_depto
-    AND poblacion.fk_id_municipio = municipio.id_municipio
-group by poblacion.id_poblacion
-order by suma desc
-) as tableD
-group by PAISS
-
-
-
-LIMIT 1
-;
 
 
 
@@ -235,6 +198,68 @@ where PAIS.ID_PAIS = REGION.FK_ID_PAIS
 AND PAIS = 'nicaragua'
 
 ;
+
+
+
+#PAIS HASTA DEPTO SOLO MUJERES Y LA SUMA DE SUS VOTOS
+select pais, region, depto, sexo.sexo, sum(poblacion.alfabeto + poblacion.analfabeto) as votos
+from pais, region, depto, municipio,
+	 poblacion,
+     sexo, detalle_sexo
+where 	#comprobacion desde pais hasta municipio 
+		pais.id_pais = region.fk_id_pais
+	and region.id_region = depto.fk_id_region
+	and municipio.fk_id_depto = depto.id_depto
+    
+		# POBLACION 
+    and poblacion.fk_id_municipio = municipio.id_municipio
+		
+        # SEXO Y DETALLE SEXO
+	and detalle_sexo.fk_id_poblacion = poblacion.id_poblacion
+    and detalle_sexo.fk_id_sexo = sexo.id_sexo
+    and sexo.sexo = 'mujeres'
+    group by depto.depto
+    order by id_poblacion
+;
+
+
+# TOTAL DE VOTOS DE MUJERES POR PAIS
+select pais, sum(poblacion.alfabeto + poblacion.analfabeto) as total
+from pais, region, depto, municipio,
+	 poblacion,
+     sexo, detalle_sexo
+where 	#comprobacion desde pais hasta municipio 
+		pais.id_pais = region.fk_id_pais
+	and region.id_region = depto.fk_id_region
+	and municipio.fk_id_depto = depto.id_depto
+    
+		# POBLACION 
+    and poblacion.fk_id_municipio = municipio.id_municipio
+		
+        # SEXO Y DETALLE SEXO
+	and detalle_sexo.fk_id_poblacion = poblacion.id_poblacion
+    and detalle_sexo.fk_id_sexo = sexo.id_sexo
+    and sexo.sexo = 'mujeres'
+    group by pais.pais
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 USE BASES1P2;
